bf1868a7fa176ed82a4d8bc54a126957cdd52804
Neel Smith <dnsmith.neel@gmail.com>
2022-09-13 12:23:43 -0400

    Count lexical tokens

---
+++ utilities/words.jl
@@ -0,0 +1,27 @@
+using CitableBase
+using CitableText
+using CitableCorpus
+using Orthography
+using LatinOrthography
+
+f = joinpath(pwd(), "texts", "latin23", "hyginus.cex")
+c = fromcex(f, CitableTextCorpus,FileReader)
+o = latin23()
+
+
+
+
+tkns = tokenize(c, o)
+
+lexprs = filter(pr -> pr[2] == LexicalToken(), tkns)
+lexstrs = map(pr -> pr[1].text, lexprs)
+grouped = group(lexstrs)
+counts = []
+for k in keys(grouped)
+    push!(counts, (k, length(grouped[k])))
+end
+sorted = sort(counts, by = pr -> pr[2], rev = true)
+
+open("wordcounts.csv", "w") do io
+    write(io, join(map(pr -> string(pr[1],",",pr[2]), sorted) ,"\n"))
+end
